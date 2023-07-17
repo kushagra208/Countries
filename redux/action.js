fa5b383e4772905b2552334c3a68979b301c1c97
kushagra208 <kushagrag2082002@gmail.com>
2023-07-17 11:32:16 +0530

    change server link

--- redux/action.js
+++ redux/action.js
@@ -28,7 +28,7 @@ import { loadUserFailure,
    } from "./authReducer.js";
 import axios from "axios"
 
-const serverUrl = "http://192.168.1.3:4000/api/v1";
+const serverUrl = "https://country-app-server.vercel.app//api/v1";
 
 export const login = (email, password) => async (dispatch) => {
   try{
